86e0e4dea6efa45612e4b1117c8858fbc4d126e9
Yusuf Bıyık <yussuf.jpeg@gmail.com>
2025-05-11 22:33:30 +0300

    Update cors

--- functions/cors.js
+++ functions/cors.js
@@ -12,7 +12,7 @@ export const onRequestOptions = async () => {
 
 export const onRequest = async (context) => {
     const response = await context.next();
-    response.headers.set("Access-Control-Allow-Origin", "http://localhost:3000");
+    response.headers.set("Access-Control-Allow-Origin", "*");
     response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
     return response;
