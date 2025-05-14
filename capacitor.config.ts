63a1f6421c19a3995b488d1ea823fce58e1af90c
Yusuf Bıyık <yussuf.jpeg@gmail.com>
2025-05-14 22:33:15 +0300

    Fix request problems

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.label_uploader.app',
   appName: 'label-upload-ui',
-  webDir: 'build'
+  webDir: 'build',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    },
+  }
 };
 
 export default config;
